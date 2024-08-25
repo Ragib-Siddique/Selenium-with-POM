4fa8d8783029aa14342effaed2320134037c1803
Ragib <ragib2789@gmail.com>
2024-08-25 22:34:00 +0600

    Selenium with POM

--- src/test/java/com/parabank/parasoft/pages/OpenNewAccountPage.java
+++ src/test/java/com/parabank/parasoft/pages/OpenNewAccountPage.java
@@ -18,7 +18,7 @@ public class OpenNewAccountPage extends BasePage {
 
 
     public OpenNewAccountPage selectAccountNumber(int index){
-        new Select(getWebElement(By.id("fromAccountId"))).selectByIndex();
+        new Select(getWebElement(By.id("fromAccountId"))).selectByIndex(index);
 
         return this;
     }
